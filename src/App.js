5a2b4731ea924a1205d135a37c9464e18e9901bc
Juan Cruz Bidegain <juancruzbidegain@gmail.com>
2021-01-22 18:47:28 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import React, {useState, useEffect} from "react"
+import Coin from "./Coins"
+import './App.css';
+import axios from "axios"
+
+
+function App() {
+  const [coins, setCoins] = useState([])
+  const [search, setSearch] = useState('')
+
+  useEffect(() => {
+    axios.get("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false")
+    .then(res =>{
+      setCoins(res.data)
+    })
+  },[])
+
+  const handleChange = e => {
+    setSearch(e.target.value)
+  }
+
+  const filterCoins = coins.filter(coin => coin.name.toLowerCase().includes(search.toLowerCase()))
+
+  return (
+    <div className="coin-app">
+      <div className="coin-search">
+        <h1 className="coin-text">
+          Buscador Crypto 💰
+        </h1>
+        <form action="">
+          <input 
+          type="text"
+          placeholder="Search"
+          className="coin-input"
+          onChange={handleChange}
+          />
+        </form>
+      </div>
+      {filterCoins.map(coin => {
+        return(
+          <Coin key={coin.id}
+          name={coin.name}
+          image={coin.image}
+          symbol={coin.symbol}
+          volume={coin.market_cap}
+          price={coin.current_price}
+          priceChange={coin.price_change_percentage_24h}
+          marketcap={coin.total_volume}
+          />
+        )
+      })}
+    </div>
+  );
+}
+
+export default App;
